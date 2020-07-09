13991b012c31026d2d4e587ef68de7cc29278ca3
Robert Schlegel <3503570@myuwc.ac.za>
2020-07-09 17:59:23 +0000

    Ready to begin accessing data

--- code/functions.R
+++ code/functions.R
@@ -2,6 +2,15 @@
 # The purpose of this script is to house functions used with the iAtlantic data
 
 
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+library(tidync)
+library(heatwaveR)
+library(doParallel)
+registerDoParallel(cores = 48)
+
+
 # Load --------------------------------------------------------------------
 
 
